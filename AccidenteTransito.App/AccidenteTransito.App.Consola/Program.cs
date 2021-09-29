9e93f775e494f825cbbec46ab18867e9a9aa15f1
YheisonReyes <yheisonreyes@gmail.com>
2021-09-29 09:59:04 -0500

    Iteracción DB

--- AccidenteTransito.App/AccidenteTransito.App.Consola/Program.cs
+++ AccidenteTransito.App/AccidenteTransito.App.Consola/Program.cs
@@ -6,9 +6,31 @@ namespace AccidenteTransito.App.Consola
 {
     class Program
     {
+        private static InterfaceRepositorioPersona _repoPersona=new RepositorioPersona(new Persistencia.AppContext());
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World desde Accidente de Transito!");
+            //AddPersona();
+            BuscarPersona(1);
+        }
+
+        private static void AddPersona(){
+            var persona=new Persona(){
+                Documento="14327",
+                Nombres="Enrique",
+                Apellidos="Romero",
+                Edad=35,
+                Genero=Genero.Masculino,
+                Telefono="314258",
+                Email="enrique@c#.com.co"
+            };
+            _repoPersona.AddPersona(persona);
+        }
+
+        private static void BuscarPersona(int idPersona){
+            var persona=_repoPersona.GetPersona(idPersona);
+            Console.WriteLine(persona.Nombres+" "+persona.Apellidos);
         }
     }
 }
