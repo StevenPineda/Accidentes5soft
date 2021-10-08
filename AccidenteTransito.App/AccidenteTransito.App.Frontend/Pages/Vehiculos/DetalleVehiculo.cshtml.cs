fafad325b58e9f3767c1976c23ed6e1f9d26b35c
JuliMontes <jamontesg@uqvirtual.edu.co>
2021-10-08 16:29:30 -0500

    Frontend 2.2

---
+++ AccidenteTransito.App/AccidenteTransito.App.Frontend/Pages/Vehiculos/DetalleVehiculo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AccidenteTransito.App.Frontend.Pages
+{
+    public class DetalleVehiculoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
